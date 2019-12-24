465eab01772aef9e5615455f7f8181e3b048ba05
ZhaoWenHao1 <31342077+ZhaoWenHao1@users.noreply.github.com>
2019-12-24 14:08:37 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,18 @@
 module.exports = {
-    plugins: [
-        {
-            resolve: `gatsby-theme-gine-blog`,
-            options: {
-                configTable: "https://www.notion.so/b4af861710064848aaf9f859c79bb127?v=0de244d6f8414ade9b7b147adcb2f78e"
-            }
-        },
-        {
-            resolve: `gatsby-source-notion-database`,
-            options: {
-                configTable: "https://www.notion.so/65b35da1762e4f259904bb4cc38e54fd?v=89e80623e52e4419a35f20e54056cfb8"
-            }
-        }
-    ],
+  plugins: [
+    {
+      resolve: `gatsby-theme-gine-blog`,
+      options: {
+        configTable:
+            'https://www.notion.so/happy0101/62067c6644194669a9436a524bc71859?v=dcbcf36987c44a4d9fe9b23c17f27287'
+      }
+    },
+    {
+      resolve: `gatsby-source-notion-database`,
+      options: {
+        configTable:
+            'https://www.notion.so/happy0101/4025fe82ef124be09a68b012494bd8b3?v=ab3535e0d35640a696afed48d028028a'
+      }
+    }
+  ],
 }
