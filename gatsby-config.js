d79aaa9a19a08f953ae93d67c22162fb4c79ab63
ZhaoWenHao1 <31342077+ZhaoWenHao1@users.noreply.github.com>
2019-12-25 11:42:23 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,15 +4,16 @@ module.exports = {
       resolve: `gatsby-theme-gine-blog`,
       options: {
         configTable:
-            'https://www.notion.so/happy0101/bd2b4afd240f4fb08caf1b10c27a8fe5?v=82f2913f950847fdb795adfe62279b99'
+            'https://www.notion.so/happy0101/c25e538627e342cc9d91bd3f4be9e27e?v=5eba2e73e33e4e37b18dfc803544c1e2'
+      
       }
     },
     {
       resolve: `gatsby-source-notion-database`,
       options: {
         configTable:
-            'https://www.notion.so/happy0101/c25e538627e342cc9d91bd3f4be9e27e?v=5eba2e73e33e4e37b18dfc803544c1e2'
-      }
+            'https://www.notion.so/happy0101/bd2b4afd240f4fb08caf1b10c27a8fe5?v=82f2913f950847fdb795adfe62279b99'
+      }    
     }
   ],
 }
